55b312a5a29a261495c659e84d70b02163c9a56c
Bruno Kucevic <brunokucevic@gmail.com>
2020-05-20 18:45:05 +0200

    Initial Commit

---
+++ RatingzAppp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingzAppp
+//
+//  Created by Bruno Kucevic on 20/05/2020.
+//  Copyright © 2020 Bruno Kucevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
